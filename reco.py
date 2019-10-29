3ebdc4f0e7cfad05546cd3ee385e0e8297f4da74
Olzhas Askar <olzhas.askar@gmail.com>
2019-10-29 09:56:41 +0100

    Add server

---
+++ reco.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+@app.route('/new', methods=['POST'])
+def send_swimlanes():
+    json = request.get_json()
+    print(json)
+    socketio.emit('swimlanes', json)
+
+    res = make_response(jsonify({'message': 'Successful!'}), 200)
+    return res
+
+
+if __name__ == '__main__':
+    socketio.run(app)
