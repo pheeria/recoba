1127ed14d05399454624b687acb3489828f86f5e
Olzhas Askar <olzhas.askar@gmail.com>
2019-10-31 23:18:53 +0100

    Change format

--- models/transformations.py
+++ models/transformations.py
@@ -1,21 +1,28 @@
 from user_agents import parse
+
+
 def transform(req):
     try:
         useragent = parse(req["user_agent"])
     except:
         useragent = None
-    friendly_useragent = 'web'
-    if useragent and useragent.is_mobile and useragent.os.family == 'iOS':
-        friendly_useragent = 'apple'
-    if useragent and useragent.is_mobile and useragent.os.family == 'Android':
-        friendly_useragent = 'android'
+    friendly_useragent = "web"
+    if useragent and useragent.is_mobile and useragent.os.family == "iOS":
+        friendly_useragent = "apple"
+    if useragent and useragent.is_mobile and useragent.os.family == "Android":
+        friendly_useragent = "android"
+    if useragent == "android":
+        friendly_useragent = "android"
+
     return {
-        "brand": req['brand'],
+        "id": req["request_id"],
+        "brand": req["brand"],
         "location": {
             "lng": req["customer"]["lat"],
             "lat": req["customer"]["lon"]
         },
-        "count": len(req["swimlanes"]),
+        "count": len([s for s in req["swimlanes"] if len(s["restaurant_ids"]) > 0]),
+        "total": len(req["swimlanes"]),
         "platform": friendly_useragent,
-        "originalObj": req
+        "original": req
     }
--- reco.py
+++ reco.py
@@ -4,27 +4,29 @@ from flask_socketio import SocketIO
 from models.transformations import transform
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+app.config["SECRET_KEY"] = "vnkdjnfjknfl1232#"
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins='*', cookie=None)
 
 
-@app.route('/')
+@app.route("/")
 def salem():
     return "Sälem, Älem!"
 
 
-@app.route('/new', methods=['POST'])
+@app.route("/new", methods=['POST'])
 def send_swimlanes():
     json = request.get_json()
     print(json)
     try:
         swimlanes_request = transform(json)
-        socketio.emit('swimlanes', swimlanes_request)
-        res = make_response(jsonify({'message': 'Successful!'}), 200)
+        socketio.emit("swimlanes", swimlanes_request)
+        res = make_response(jsonify({"message": "Successful!"}), 200)
     except:
-        res = make_response(jsonify({'message': 'Couldn\'t parse response'}), 400)
+        res = make_response(
+            jsonify({"message": "Couldn't parse response"}), 400)
     return res
 
-if __name__ == '__main__':
-    socketio.run(app, port=8000)
+
+if __name__ == "__main__":
+    socketio.run(app)
